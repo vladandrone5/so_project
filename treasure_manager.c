ebcb9279635dab0d241ba8109e8a0ea6c4a1ca0f
Vlad Androne <vlad05androne@gmail.com>
2025-04-06 11:42:53 +0300

    commit 06 04 2025 11:42:41

--- treasure_manager.c
+++ treasure_manager.c
@@ -8,8 +8,11 @@
 #include <dirent.h>
 
 #define ARRAY_SIZE 400
+#define PATH_SIZE 256
 #define NAME_SIZE 100
 #define LINE_SIZE 1000
+
+//for testing purposes for the moment will be one treasure per file
 
 typedef enum {EXISTS, NON_EXISTENT}status_code;
 
@@ -39,11 +42,11 @@ void usage(char *name) {
     exit(-1);
 }
 
-void add(char *hunt_id);
-void list(char *hunt_id);
-void view(char *hunt_id, char *treasure_id);
-void remove_treasure(char *hunt_id, char *id);
-void remove_hunt(char *hunt_id);
+void add(char *hunt_id); // adds new treasure to the specified hunt
+void list(char *hunt_id); // list all treasures in specified hunt
+void view(char *hunt_id, char *treasure_id); // view details of specific treasure
+void remove_treasure(char *hunt_id, char *id); // removes a treasure
+void remove_hunt(char *hunt_id); // removes a hunt
 
 hunt *create_hunt(char *name) {
     hunt *tmp = malloc(sizeof(hunt));
@@ -58,6 +61,39 @@ hunt *create_hunt(char *name) {
     }
 
     tmp->h_code = EXISTS;
+
+    DIR *dir;
+    if((dir = opendir(name)) == NULL) {
+        perror("error opening directory\n");
+        exit(-1);
+    }
+
+    //creating a log part
+    strcpy(tmp->log, "log_");
+    strcat(tmp->log, name);
+    strcat(tmp->log, ".txt");
+    tmp->log[strlen(tmp->log)] = '\0';
+
+    char filepath[PATH_SIZE];
+    snprintf(filepath, sizeof(filepath), "%s/%s", name, tmp->log); // creating the filepath
+
+    int fd = open(filepath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if(fd == -1) {
+        perror("error opening log file\n");
+        exit(-1);
+    }
+
+    char *text = "LOG\n";
+    ssize_t bytes_written = write(fd, text, strlen(text));
+    if(bytes_written == -1) {
+        perror("error writting to log file\n");
+        close(fd);
+        exit(-1);
+    }
+
+    close(fd);
+
+    closedir(dir);
 
     return tmp;
 }
@@ -80,16 +116,68 @@ void list(char *hunt_id) {
     return;
 }
 
-int main(int argc, char **argv) {
-    if(argc < 3) {
-        usage(argv[0]);
+int has_files_in_directory(char *dirpath) {
+    DIR *dir;
+    if((dir = opendir(dirpath)) == NULL) {
+        perror("error opening directory\n");
+        exit(-1);
     }
+
+    struct dirent *entry;
+    while((entry = readdir(dir)) != NULL) {
+        if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+            closedir(dir);
+            return 1;
+        }
+    }
+
+    closedir(dir);
+    return 0;
+}
+
+void delete_files(char *directory_path) {
+    DIR *dir;
+    if((dir = opendir(directory_path)) == NULL) {
+        perror("error opening directory\n");
+        exit(-1);
+    }
+
+    struct dirent *entry;
+    char file_path[256];
+
+    while((entry = readdir(dir)) != NULL) {
+        if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        snprintf(file_path, sizeof(file_path), "%s/%s", directory_path, entry->d_name);
+
+        if(unlink(file_path) == -1) {
+            perror("error deleting file\n");
+            exit(-1);
+        }
+    }
+
+    closedir(dir);
+
+    return;
+}
+
+void remove_hunt(char *hunt_id) {
+    delete_files(hunt_id);
+    rmdir(hunt_id);
+    return;
+}
+
+int main(void) {
 
     //hunt *huntRef = NULL;
     //huntRef = create_hunt_directory("hunt1");
     //printf("%d\n", check_hunt_status(huntRef, "hunt1"));
 
-    
+    //hunt *h = NULL;
+    //h = create_hunt("Hunt001");
+    remove_hunt("Hunt001");
 
     return 0;
 }
